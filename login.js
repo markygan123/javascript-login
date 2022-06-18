4e4ffe5c8470a3f738cf2e017c806ef8b5ae5f94
markygan123 <macawilemarkygan@gmail.com>
2022-06-18 01:40:13 -0500

    initial commit

---
+++ login.js
@@ -0,0 +1,37 @@
+form.addEventListener('submit', (e) => {
+    const uid = localStorage.getItem("UID");
+    const pw = localStorage.getItem("PWORD");
+    const userId = document.getElementById("userId");
+    const password = document.getElementById("password");
+    const errorMsg = document.getElementById("error");
+
+    const loginBtn = document.getElementById("login");
+    let messages = [];
+
+    let success = () => {        
+        loginBtn.innerText = 'Restart';
+        userId.disabled = true;
+        password.disabled = true;
+    
+        loginBtn.addEventListener('click', (e) => {
+            localStorage.clear();
+            location.href = '/';
+        });    
+    }
+
+    if (userId.value === uid && password.value === pw) {
+        messages.push('Login successful');
+        success();        
+    } else if (userId.value != uid) {
+        messages.push('User not found');
+    } else {
+        messages.push('Wrong Password!');
+    }
+    
+    if (messages.length > 0) {
+        e.preventDefault();
+        errorMsg.innerText = messages.join(', ');
+    }
+
+});
+
---
+++ register.js
@@ -0,0 +1,41 @@
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    const userId = document.getElementById("userId");
+    const password = document.getElementById("password");
+    const form = document.getElementById("form");
+    const errorMsg = document.getElementById("error");
+    
+    const registerButton = document.getElementById("register");
+    let messages = [];
+    
+
+    let registerUser = () => {
+        let uid = localStorage.setItem("UID", userId.value);
+        let pword = localStorage.setItem("PWORD", password.value);
+        
+        return credentials = {
+            uid, pword
+        }
+    }    
+    
+    if (password.value.length < 6) {
+        messages.push('Password must be longer than 6 characters');
+    } else if (password.value.length >= 20) {
+        messages.push('Password must be not longer than 20 characters');
+    } else if (password.value === "password") {
+        messages.push(`Password cannot be 'password'`);
+    } else if (userId.value === password.value) {
+        messages.push('User ID and Password cannot be the same!')
+    }
+    
+    if (messages.length > 0) {
+        e.preventDefault()
+        errorMsg.innerText = messages.join(', ');
+    } else {
+        registerUser();
+        location.href = '/login.html'
+    }
+    
+    
+})
