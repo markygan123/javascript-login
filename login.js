5cd8877fd21d3c54b426f4f51b95a8fe0c167116
markygan123 <macawilemarkygan@gmail.com>
2022-06-18 02:23:48 -0500

    redirect to index.html after login

--- login.js
+++ login.js
@@ -15,7 +15,7 @@ form.addEventListener('submit', (e) => {
     
         loginBtn.addEventListener('click', (e) => {
             localStorage.clear();
-            location.href = '/index.html';
+            location.href = './javascript-login/index.html';
         });    
     }
 
